25f1af77a165a22d8474ce75ce60e9d350bce9e4
eric <erick_lich@hotmail.com>
2019-02-20 12:06:55 -0300

    Agrego el p al body

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let posicion = ubicacion => {
-    let lat =ubicacion.coords.latitude
+    let lat = ubicacion.coords.latitude
     let lon = ubicacion.coords.longitude
     let p = document.createElement("p")
     p.innerText = `Mi ubicacion es ${lat} y ${lon}`
+    document.body.appendChild(p)
 }
 
 navigator.geolocation.getCurrentPosition(posicion)
